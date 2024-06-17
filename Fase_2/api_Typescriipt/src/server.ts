76f1ebee55fb35f970d0bb5d805ca61292297331
escuderodev <escudero.ti@gmail.com>
2024-06-16 21:31:03 -0300

    feat: att

--- Fase_2/api_Typescriipt/src/server.ts
+++ Fase_2/api_Typescriipt/src/server.ts
@@ -1,9 +1,11 @@
-import { app } from '/app';
-import { env } from './env';
+import { env } from '@/env'
+import { app } from '@/app'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
     port: env.PORT,
-}).then(() => {
-    console.log('Server is running on http://localhost:3000');
-});
+  })
+  .then(() => {
+    console.log('Server is running on http://localhost:3000')
+  })
